4c0709d064014877f664f3ab18f93c5047619669
kevinluo <kevinluo@anuegroup.com.tw>
2025-06-25 16:54:07 +0800

    refactor: update GTLSCA certificate handling by replacing .crt with .pem and clean up catering service routes

--- src/routes/cateringservice.ts
+++ src/routes/cateringservice.ts
@@ -5,25 +5,16 @@ import fs from "fs";
 import path from "path";
 // types
 import type { Response } from "express";
-import type { RequestBody, RequestQuery } from "../types";
+import type { RequestQuery } from "../types";
 
-// const ePKICert = fs.readFileSync(path.resolve(process.cwd(), "src/assets/GTLSCA.crt"), "utf8");
-const ePKICert = fs.readFileSync(path.join(process.cwd(), "src", "assets", "GTLSCA.crt"), "utf8");
+// const ePKICert = fs.readFileSync(path.join(process.cwd(), "src", "assets", "GTLSCA.crt"), "utf8");
+const ePKICert = fs.readFileSync(path.join(process.cwd(), "src", "assets", "GTLSCA.pem"), "utf8");
 
 const httpsAgent = new https.Agent({
     ca: ePKICert,
 });
 
 const router = Router();
-
-interface RestaurantParams {
-    method: string;
-    args: {
-        schoolId: number;
-        schoolCode: string;
-        schoolName: string;
-    };
-}
 
 interface RestaurantResponse {
     result_content: {
@@ -50,28 +41,9 @@ interface RestaurantResponse {
     error_msg: string;
 }
 
-router.post("/rest/API", async (req: RequestBody<RestaurantParams>, res: Response) => {
-    try {
-        const result = await axios.post<RestaurantResponse>(
-            "https://fatraceschool.k12ea.gov.tw/cateringservice/rest/API/",
-            req.body
-        );
-
-        res.status(200).json({
-            result: Number(result.data.result),
-            message: result.data.result_content.msg,
-            data: result.data.result_content.storeList,
-        });
-    } catch (error: any) {
-        res.status(500).json({ result: 0, message: error?.message ?? "Unknown Error" });
-    }
-});
-
 router.get("/rest/test", async (req: RequestQuery<Record<string, string>>, res: Response) => {
     try {
-        // const httpsAgent = new https.Agent({
-        //     rejectUnauthorized: false,
-        // });
+        // console.log("ePKICert: ", ePKICert);
         const result = await axios.post<RestaurantResponse>(
             "https://fatraceschool.k12ea.gov.tw/cateringservice/rest/API/",
             {
@@ -83,26 +55,6 @@ router.get("/rest/test", async (req: RequestQuery<Record<string, string>>, res: 
                 },
             },
             {
-                // headers: {
-                //     Cookie: `JSESSIONID=${req.query.JSESSIONID}`,
-                //     "User-Agent":
-                //         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                //     Accept: "application/json, text/plain, */*",
-                //     "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8",
-                //     "Accept-Encoding": "gzip, deflate, br",
-                //     "Content-Type": "application/json",
-                //     Origin: "https://fatraceschool.k12ea.gov.tw",
-                //     Referer: "https://fatraceschool.k12ea.gov.tw/",
-                //     DNT: "1",
-                //     Connection: "keep-alive",
-                //     "Sec-Fetch-Dest": "empty",
-                //     "Sec-Fetch-Mode": "cors",
-                //     "Sec-Fetch-Site": "same-origin",
-                //     "X-Requested-With": "XMLHttpRequest",
-                // },
-                // timeout: 30000, // 30 seconds timeout
-                // maxRedirects: 5,
-                // validateStatus: (status) => status < 500, // Accept all status codes below 500
                 httpsAgent,
             }
         );
